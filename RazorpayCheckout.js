44b5582775044f71bbddb281767f85199f43b45c
tarunmangal <tarunmangal@gmail.com>
2017-12-18 10:33:40 +0530

    bug fixed: if NativeModule exist or not

--- RazorpayCheckout.js
+++ RazorpayCheckout.js
@@ -1,37 +1,47 @@
 'use strict';
 
-import { NativeModules, NativeEventEmitter } from 'react-native';
+import {NativeModules, NativeEventEmitter} from 'react-native';
 
-const razorpayEvents = new NativeEventEmitter(NativeModules.RazorpayEventEmitter);
+const razorpayEvents = (NativeModules !== null) ? new NativeEventEmitter(NativeModules.RazorpayEventEmitter) : undefined;
 
-const removeSubscriptions = () => {
-  razorpayEvents.removeAllListeners('Razorpay::PAYMENT_SUCCESS');
-  razorpayEvents.removeAllListeners('Razorpay::PAYMENT_ERROR');
-  razorpayEvents.removeAllListeners('Razorpay::EXTERNAL_WALLET_SELECTED');
-};
+const removeSubscriptions = () =
+>
+{
+    razorpayEvents.removeAllListeners('Razorpay::PAYMENT_SUCCESS');
+    razorpayEvents.removeAllListeners('Razorpay::PAYMENT_ERROR');
+    razorpayEvents.removeAllListeners('Razorpay::EXTERNAL_WALLET_SELECTED');
+}
+;
 
 class RazorpayCheckout {
-  static open(options, successCallback, errorCallback) {
-    return new Promise(function(resolve, reject) {
-      razorpayEvents.addListener('Razorpay::PAYMENT_SUCCESS', (data) => {
-        let resolveFn = successCallback || resolve;
-        resolveFn(data);
-        removeSubscriptions();
-      });
-      razorpayEvents.addListener('Razorpay::PAYMENT_ERROR', (data) => {
-        let rejectFn = errorCallback || reject;
-        rejectFn(data);
-        removeSubscriptions();
-      });
-      NativeModules.RazorpayCheckout.open(options);
-    });
-  }
-  static onExternalWalletSelection(externalWalletCallback) {
-    razorpayEvents.addListener('Razorpay::EXTERNAL_WALLET_SELECTED', (data) => {
-      externalWalletCallback(data);
-      removeSubscriptions();
-    });
-  }
+    static open(options, successCallback, errorCallback) {
+        if (razorpayEvents) {
+            return new Promise(function (resolve, reject) {
+                razorpayEvents.addListener('Razorpay::PAYMENT_SUCCESS', (data) => {
+                    let resolveFn = successCallback || resolve;
+                    resolveFn(data);
+                    removeSubscriptions();
+                });
+                razorpayEvents.addListener('Razorpay::PAYMENT_ERROR', (data) => {
+                    let rejectFn = errorCallback || reject;
+                    rejectFn(data);
+                    removeSubscriptions();
+                });
+                NativeModules.RazorpayCheckout.open(options);
+            });
+        } else {
+            return undefined;
+        }
+    }
+
+    static onExternalWalletSelection(externalWalletCallback) {
+        if (razorpayEvents) {
+            razorpayEvents.addListener('Razorpay::EXTERNAL_WALLET_SELECTED', (data) => {
+                externalWalletCallback(data);
+                removeSubscriptions();
+            });
+        }
+    }
 }
 
 export default RazorpayCheckout;
