74db64340cf31202bde0ca8b82cd9dcfbfc3bedd
Kashyap Paidimarri <kashyapp@gmail.com>
2017-12-19 15:52:35 +0530

    Update download_ios_framework.sh

--- download_ios_framework.sh
+++ download_ios_framework.sh
@@ -2,6 +2,6 @@
 ## Run this script after installing the module
 
 curl -O http://downloads.razorpay.com/Razorpay.framework-0.15.1-bitcode.zip
-unzip -o ./Razorpay.framework-0.15.1-bticode.zip
+unzip -o ./Razorpay.framework-0.15.1-bitcode.zip
 rm -r ./ios/Razorpay.framework
 cp -R ./Razorpay.framework ./ios/
