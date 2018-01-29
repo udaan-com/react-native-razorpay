25be3530e658c73f78ffe5a3fb76de8d600bbea4
Aakash Bapna <aakash.bapna@gmail.com>
2018-01-29 19:41:59 +0530

    updating download url for bitcode.zip
    got the url from here- https://github.com/razorpay/razorpay-ios-sample-app/releases

--- download_ios_framework.sh
+++ download_ios_framework.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 ## Run this script after installing the module
 
-curl -O http://downloads.razorpay.com/Razorpay.framework-0.15.1-bitcode.zip
+curl -O https://github.com/razorpay/razorpay-ios-sample-app/releases/download/0.15.1/Razorpay.framework-0.15.1-bitcode.zip
 unzip -o ./Razorpay.framework-0.15.1-bitcode.zip
 rm -r ./ios/Razorpay.framework
 cp -R ./Razorpay.framework ./ios/
