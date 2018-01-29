45dfee600833c4c051595a12d5b6646e3d4c0c64
Aakash Bapna <aakash.bapna@gmail.com>
2018-01-29 19:50:49 +0530

    adding set -e and debug statements

--- download_ios_framework.sh
+++ download_ios_framework.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
+set -e
 ## Run this script after installing the module
-
+echo "fetching Razorpay.framework-0.15.1-bitcode.zip"
 curl -O https://github.com/razorpay/razorpay-ios-sample-app/releases/download/0.15.1/Razorpay.framework-0.15.1-bitcode.zip
 unzip -o ./Razorpay.framework-0.15.1-bitcode.zip
 rm -r ./ios/Razorpay.framework
