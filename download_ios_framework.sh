964c31d6823ae20cd2b7c0c556d8fa4f92726e36
Aakash Bapna <aakash.bapna@gmail.com>
2018-01-29 19:57:43 +0530

    Adding -L flag to curl to follow redirects and download the file

--- download_ios_framework.sh
+++ download_ios_framework.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -e
 ## Run this script after installing the module
-echo "fetching Razorpay.framework-0.15.1-bitcode.zip"
-curl -O https://github.com/razorpay/razorpay-ios-sample-app/releases/download/0.15.1/Razorpay.framework-0.15.1-bitcode.zip
+echo "fetching Razorpay.framework-0.15.1-bitcode.zip ..."
+curl -L -O https://github.com/razorpay/razorpay-ios-sample-app/releases/download/0.15.1/Razorpay.framework-0.15.1-bitcode.zip
 unzip -o ./Razorpay.framework-0.15.1-bitcode.zip
 rm -r ./ios/Razorpay.framework
 cp -R ./Razorpay.framework ./ios/
