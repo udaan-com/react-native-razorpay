1cc020c1d888e14ce3a1ce1368e7a74a9e3788de
Tarun Mangal <tarunmangal@gmail.com>
2018-02-01 00:22:04 +0530

    Changed the name of the file to RazorpayBitcodeX9

--- download_ios_framework.sh
+++ download_ios_framework.sh
@@ -2,8 +2,8 @@
 ## Run this script after installing the module
 
 curl -O https://rzp-mobile.s3.amazonaws.com/ios/checkout/1.0.8/RazorpayBitcodeX9.framework.zip 
-unzip -o ./RazorpayX9.framework.zip
+unzip -o ./RazorpayBitcodeX9.framework.zip
 rm -r ./ios/Razorpay.framework
 cp -r ./Razorpay.framework ./ios/
 rm -r Razorpay.framework
-rm RazorpayX9.framework.zip
+rm RazorpayBitcodeX9.framework.zip
