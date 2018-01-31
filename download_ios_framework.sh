a6b676c059a59b053f150e7865c6666ad3752ebf
tarunmangal <tarunmangal@gmail.com>
2018-01-31 17:55:04 +0530

    Updated the URL to latest bytecode

--- download_ios_framework.sh
+++ download_ios_framework.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 ## Run this script after installing the module
 
-curl -O https://rzp-mobile.s3.amazonaws.com/ios/checkout/1.0.8/RazorpayX9.framework.zip
+curl -O https://rzp-mobile.s3.amazonaws.com/ios/checkout/1.0.8/RazorpayBitcodeX9.framework.zip 
 unzip -o ./RazorpayX9.framework.zip
 rm -r ./ios/Razorpay.framework
 cp -r ./Razorpay.framework ./ios/
