b52f5681330d1c8ea74b9fe35a59d595c83c493d
tarunmangal <tarunmangal@gmail.com>
2018-03-01 21:20:23 +0530

    moved to the lastest version of razorpay framework

--- download_ios_framework.sh
+++ download_ios_framework.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 ## Run this script after installing the module
 
-curl -O https://dl.dropboxusercontent.com/content_link/P0aWzGoCxvxMZnU3ednT3XjSuXFgJa29Az5tOi222HjvvJYJeiCn81xyoolVOLRw/file?dl=1 
-unzip -o ./Razorpay.framework-0.15.1-bitcode.zip
+curl -O https://rzp-mobile.s3.amazonaws.com/ios/checkout/1.0.8/RazorpayBitcodeX9.framework.zip
+unzip -o ./RazorpayBitcodeX9.framework.zip
 rm -r ./ios/Razorpay.framework
 cp -r ./Razorpay.framework ./ios/
 rm -r Razorpay.framework
-rm Razorpay.framework-0.15.1-bitcode.zip
+rm RazorpayBitcodeX9.framework.zip
